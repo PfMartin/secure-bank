e5763425ff02551632d498ba58fbedd3e438112a
PfMartin <martinpfatrisch@gmail.com>
2023-11-22 07:55:00 +0100

    Created test for get account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,12 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/PfMartin/secure-bank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner: util.RandomOwner(),
 		Balance: util.RandomMoney(),
@@ -25,4 +26,24 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	created_account := createRandomAccount(t)
+	got_account, err := testQueries.GetAccount(context.Background(), created_account.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, got_account)
+
+	require.Equal(t, created_account.ID, got_account.ID)
+	require.Equal(t, created_account.Owner, got_account.Owner)
+	require.Equal(t, created_account.Balance, got_account.Balance)
+	require.Equal(t, created_account.Currency, got_account.Currency)
+	require.WithinDuration(t, created_account.CreatedAt.Time, got_account.CreatedAt.Time, time.Second)
 }
