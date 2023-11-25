fc8c117ce6489d713baef3d9b48b50b9b821d014
PfMartin <martinpfatrisch@gmail.com>
2023-11-26 00:51:53 +0100

    Reads config with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,22 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/PfMartin/secure-bank/util"
 	_ "github.com/lib/pq"
-)
-
-// const connString = "host=localhost user=root password=secret dbname=secure_bank sslmode=disable"
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/secure_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db: ", err)
 	}
