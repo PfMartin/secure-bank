c620c8a6d15cd0ab4eb6edf762f4b450bbc95335
Martin Pfatrisch <martin.pfatrisch@kinexon.com>
2023-11-29 17:17:41 +0100

    Added auth middleware

--- api/server.go
+++ api/server.go
@@ -40,13 +40,15 @@ func (server *Server) SetupRouter() {
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccount)
 	// TODO: Update account route
 	// TODO: Delete account route
 
-	router.POST("/transfers", server.createTransfer)
+	authRoutes.POST("/transfers", server.createTransfer)
 
 	server.router = router
 }
